94d33b29c423879f1e43e2b60af5b7b652041136
she_chengdong <she_chengdong@hoperun.com>
2026-08-11 06:59:36 +0000

    同步format/usage
    Signed-off-by: she_chengdong <she_chengdong@hoperun.com>

--- p7885/hardware/display/src/display_device/core/hdi_fence_trace.h
+++ p7885/hardware/display/src/display_device/core/hdi_fence_trace.h
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2023 HiHope Open Source Organization.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 #ifndef HDI_FENCE_TRACE_H
 #define HDI_FENCE_TRACE_H
 
@@ -109,7 +95,7 @@ inline int32_t CountProcessFds()
     int32_t count = 0;
     for (dirent* entry = readdir(dir); entry != nullptr; entry = readdir(dir)) {
         if (entry->d_name[0] == '.' &&
-            (entry->d_name[1] == '\0' || strcmp(entry->d_name, "..") == 0)) {
+            (entry->d_name[1] == '\0' || (entry->d_name[1] == '.' && entry->d_name[2] == '\0'))) {
             continue;
         }
         ++count;
--- p7885/hardware/display/src/display_gralloc/hi_gbm_internal.h
+++ p7885/hardware/display/src/display_gralloc/hi_gbm_internal.h
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2023 HiHope Open Source Organization.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 #ifndef P7885_HI_GBM_INTERNAL_H
 #define P7885_HI_GBM_INTERNAL_H
 
@@ -22,19 +8,6 @@
 #define HEIGHT_ALIGN_GBM 2U
 #define WIDTH_ALIGN_GBM 8U
 #define MAX_PLANES_GBM 3
-
-struct PlaneLayoutInfo {
-    uint32_t numPlanes;
-    uint32_t ratio[MAX_PLANES_GBM];
-};
-
-struct FormatInfo {
-    uint32_t format;
-    uint32_t bitsPerPixel;
-    const PlaneLayoutInfo *planes;
-};
-
-const FormatInfo *GetFormatInfo(uint32_t format);
 
 struct gbm_device {
     int fd;
